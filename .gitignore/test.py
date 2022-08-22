9b721f7d5fc7997761cf0834229fd26245ccc9b8
Четин Евгений <chetinee@it-impuls.ru>
2022-08-22 22:09:55 +0300

    message abount changes

---
+++ .gitignore/test.py
@@ -0,0 +1,33 @@
+class Contact:
+    name: str
+    year_birth: int
+    is_programmer: int
+    def __init__(self,
+                 name: str,
+                 year_birth: int,
+                 is_programmer: bool) -> None:
+        self.name = name
+        self.year_birth = year_birth
+        self.is_programmer = is_programmer
+    print(__init__.__annotations__)
+    def age_define(self) -> str:
+        if 1946 < self.year_birth < 1980:
+            return 'Олдскул'
+        if self.year_birth >= 1980:
+            return 'Молодой'
+        return 'Старейшина'
+    print(age_define.__annotations__)
+    def programmer_define(self) -> str:
+        if self.is_programmer:
+            return 'Программист'
+        return 'Нормальный'
+    print(age_define.__annotations__)
+    def show_contact(self) -> str:
+        return(f'{self.name}, '
+               f'категория: {self.age_define()}, '
+               f'статус: {self.programmer_define()}')
+    print(show_contact.__annotations__)
+    def print_contact(self) -> None:
+        print(self.show_contact())
+    print(print_contact.__annotations__)
+print(Contact.__annotations__)
